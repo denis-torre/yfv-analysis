01e721ccb2ad4b95697adfff41323485467a7388
denis-torre <denis.torre@mssm.edu>
2017-02-22 18:12:57 -0500

    Added differential expression, Enrichr and L1000CDS2

--- pipeline/pipeline-yfv-analysis.py
+++ pipeline/pipeline-yfv-analysis.py
@@ -1,6 +1,6 @@
 #################################################################
 #################################################################
-###############  ################
+############### Yellow Fever Analysis ###########################
 #################################################################
 #################################################################
 ##### Author: Denis Torre
@@ -32,6 +32,7 @@ import PipelineYfvAnalysis as P
 seriesMatrixFile = 'rawdata.dir/expression/GSE51972_series_matrix.txt'
 clinicalAnnotationFile = 'rawdata.dir/annotations/YFV_clinicalparameters.xls'
 platformAnnotationFile = 'rawdata.dir/annotations/GPL3535-10024.txt'
+humanHomologyFile = 'rawdata.dir/annotations/macaque_human_homologs.txt'
 
 ##### 2. R Connection #####
 rSource = 'pipeline/scripts/pipeline-yfv-analysis.R'
@@ -48,13 +49,13 @@ r.source(rSource)
 ########## 1. Sample Annotation Data
 #############################################
 
-@follows(mkdir('f1-annotation_data.dir'))
+@follows(mkdir('f1-annotation.dir'))
 
 @merge([seriesMatrixFile,
 		clinicalAnnotationFile],
-	   'f1-annotation_data.dir/yfv-sample_annotations.txt')
+	   'f1-annotation.dir/yfv-sample_annotations.txt')
 
-def processSampleAnnotations(infiles, outfile):
+def getSampleAnnotations(infiles, outfile):
 
 	# Open file
 	with open(seriesMatrixFile, 'r') as openfile:
@@ -116,7 +117,7 @@ def processSampleAnnotations(infiles, outfile):
 #############################################
 
 @files(platformAnnotationFile,
-	   'f1-annotation_data.dir/GPL3535-gene_annotations.txt')
+	   'f1-annotation.dir/GPL3535-gene_annotations.txt')
 
 def getGeneAnnotations(infile, outfile):
 
@@ -142,11 +143,11 @@ def getGeneAnnotations(infile, outfile):
 ########## 3. Gene Expression Data
 #############################################
 
-@follows(mkdir('f2-expression_data.dir'))
+@follows(mkdir('f2-raw_expression.dir'))
 
 @files([seriesMatrixFile,
 		getGeneAnnotations],
-	   'f2-expression_data.dir/yfv-gene_expression_matrix.txt')
+	   'f2-raw_expression.dir/yfv-gene_expression.txt')
 
 def getGeneExpressionData(infiles, outfile):
 
@@ -176,17 +177,197 @@ def getGeneExpressionData(infiles, outfile):
 
 #######################################################
 #######################################################
-########## S. 
+########## S3. Run Differential Expression
 #######################################################
 #######################################################
 
 #############################################
-########## . 
+########## 1. Run Characteristic Direction
 #############################################
 
+@follows(mkdir('f3-differential_expression.dir/cd'))
 
 
+@transform(getGeneExpressionData,
+		   regex(r'.*/(.*)_expression.txt'),
+		   add_inputs(getSampleAnnotations),
+		   r'f3-differential_expression.dir/cd/\1_differential_expression.txt')
 
+def runCharacteristicDirection(infiles, outfile):
+
+	# Split infiles
+	expressionFile, annotationFile = infiles
+
+	# Read expression data
+	expressionDataframe = pd.read_table(expressionFile, index_col='gene_symbol')
+
+	# Read annotation data
+	annotationDataframe = pd.read_table(annotationFile, index_col='Sample_geo_accession')
+
+	# Reindex annotation dataframe
+	annotationDataframeReindex = annotationDataframe.reset_index().set_index(['treatment','timepoint'])
+
+	# Get unique treatments and timepoints
+	treatments = set(annotationDataframe['treatment'])
+	timepoints = set(annotationDataframe['timepoint'])
+
+	# Get sample dictionary
+	sampleDict = {'_'.join([treatment, timepoint]): annotationDataframeReindex.loc[(treatment, timepoint), 'Sample_geo_accession'].tolist() for timepoint in timepoints for treatment in treatments}
+
+	# Initialize empty dataframe
+	resultDataframe = pd.DataFrame()
+
+	# Loop through comparisons
+	for comparisons in [['17D-vaccinated_day0', '17D-vaccinated_day3'], ['YFVwt-infected_day0', 'YFVwt-infected_day3'], ['17D-vaccinated_day0', 'YFVwt-infected_day0'], ['17D-vaccinated_day3', 'YFVwt-infected_day3']]:
+	    
+		# Get columns
+		controlColumns, experimentColumns = [sampleDict[x] for x in comparisons]
+
+		# Run characteristic direction
+		cdResults = r.runCharacteristicDirection(com.convert_to_r_dataframe(expressionDataframe), experimentColumns, controlColumns, 0.01)
+
+		# Convert to dataframe
+		cdDataframe = com.convert_robj(cdResults).reset_index()
+
+		# Add comparison column
+		cdDataframe['comparison'] = 'v'.join(comparisons)
+
+		# Append
+		resultDataframe = pd.concat([resultDataframe, cdDataframe])
+
+	# Pivot
+	resultDataframeCast = resultDataframe.pivot(index='index', columns='comparison', values='CD')
+
+	# Save
+	resultDataframeCast.to_csv(outfile, sep='\t', index_label='gene_symbol')
+
+
+#######################################################
+#######################################################
+########## S4. Enrichment Analysis
+#######################################################
+#######################################################
+
+#############################################
+########## 1. Submit Genesets 
+#############################################
+
+@follows(mkdir('f4-enrichr.dir'))
+
+@transform(runCharacteristicDirection,
+		   regex(r'.*/(.*)differential_expression.txt'),
+		   r'f4-enrichr.dir/\1enrichr_links.txt')
+
+def submitEnrichrGenesets(infile, outfile):
+
+	# Read infile
+	cdDataframe = pd.read_table(infile, index_col='gene_symbol').fillna(0)
+
+	# Rewrite index
+	cdDataframe.index = [x.split(' /// ')[0] for x in cdDataframe.index]
+
+	# Initialize link dataframe
+	resultDataframe = pd.DataFrame()
+
+	# Loop through timepoints
+	for comparison in cdDataframe.columns:
+
+	    # Get Enrichr links
+	    enrichrLinkDataframe = S.uploadToEnrichr(cdDataframe, comparison)
+
+	    # Add timepoint label
+	    enrichrLinkDataframe['comparison'] = comparison
+
+	    # Concatenate
+	    resultDataframe = pd.concat([resultDataframe, enrichrLinkDataframe])
+
+	# Save data
+	resultDataframe.to_csv(outfile, sep='\t', index=False)
+
+#############################################
+########## 2. Get Enrichment results
+#############################################
+
+@transform(submitEnrichrGenesets,
+		   regex(r'.*/(.*)links.txt'),
+		   r'f4-enrichr.dir/\1results.txt')
+
+def getEnrichrResults(infile, outfile):
+
+	# Read infile
+	enrichrLinkDataframe = pd.read_table(infile, index_col=['geneset','comparison'])
+
+	# Initialize result dataframe
+	resultDataframe = pd.DataFrame()
+
+	# Set libraries
+	libraries = ['ChEA_2016', 'KEGG_2016', 'GO_Biological_Process_2015', 'GO_Cellular_Component_2015', 'GO_Molecular_Function_2015', 'VirusMINT']
+
+	# Loop through timepoints, genesets and libraries
+	for geneset in enrichrLinkDataframe.index.levels[0]:
+	    for comparison in enrichrLinkDataframe.index.levels[1]:
+	        for library in libraries:
+
+	            # Get enrichment results
+	            enrichmentResultDataframe = S.getEnrichmentResults(enrichrLinkDataframe.loc[(geneset, comparison), 'userListId'], library)
+
+	            # Add labels
+	            enrichmentResultDataframe['comparison'] = comparison
+	            enrichmentResultDataframe['geneset'] = geneset
+	            enrichmentResultDataframe['library'] = library
+
+	            # Concatenate
+	            resultDataframe = pd.concat([resultDataframe, enrichmentResultDataframe])
+
+    # Write file
+	resultDataframe.to_csv(outfile, sep='\t', index=False)
+
+
+#######################################################
+#######################################################
+########## S9. L1000CDS2 Analysis
+#######################################################
+#######################################################
+
+#############################################
+########## 1. Submit analysis
+#############################################
+
+@follows(mkdir('f5-l1000cds2.dir'))
+
+@transform(runCharacteristicDirection,
+		   regex(r'.*/(.*)differential_expression.txt'),
+		   r'f5-l1000cds2.dir/\1l1000cds2_links.txt')
+
+def runL1000CDS2(infile, outfile):
+
+	# Read infile
+	cdDataframe = pd.read_table(infile, index_col='gene_symbol').fillna(0)
+
+	# Rewrite index
+	cdDataframe.index = [x.split(' /// ')[0] for x in cdDataframe.index]
+
+	# Initialize dataframes
+	linkDataframe = pd.DataFrame()
+	signatureDataframe = pd.DataFrame()
+
+	# Loop through timepoints
+	for comparison in cdDataframe.columns:
+
+	    # Run L1000CDS2
+	    resultDict = S.getL1000CDS2Results(cdDataframe, comparison)
+
+	    # Add comparison labels
+	    resultDict['links']['comparison'] = comparison
+	    resultDict['signatures']['comparison'] = comparison
+
+	    # Append dataframes
+	    linkDataframe = pd.concat([linkDataframe, resultDict['links']])
+	    signatureDataframe = pd.concat([signatureDataframe, resultDict['signatures']])
+
+	# Write files
+	linkDataframe.to_csv(outfile, sep='\t', index=False)
+	signatureDataframe.to_csv(outfile.replace('links', 'signatures'), sep='\t', index=False)
 
 
 #######################################################
